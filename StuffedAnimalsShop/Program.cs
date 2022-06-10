f968b47551c12ab0ef8ed0612fc68e101e38156e
THOMAS MICHAEL HALLMEN <thomas.hallmen@ulbsibiu.ro>
2022-06-10 21:03:56 +0300

    stuffed animal list

--- StuffedAnimalsShop/Program.cs
+++ StuffedAnimalsShop/Program.cs
@@ -10,6 +10,17 @@ namespace StuffedAnimalsShop
     {
         static void Main(string[] args)
         {
+            var animal = new List<Animal>()
+            {
+                new Dog(),
+                new Cat(),
+                new Cow()
+            };
+
+            foreach (var stuffedAnimal in animal)
+            {
+                Console.WriteLine("The stuffed animal " + stuffedAnimal.GetType().Name + " got bought");
+            }
         }
     }
 }
